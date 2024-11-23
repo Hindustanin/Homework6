fdba3f7f3fdc0c96121d28651e502e64b9b9ec9a
Hindustanin <hindustanin@hotmail.com>
2024-11-23 23:13:40 +0700

    Старт

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,79 @@ public class Main {
         // Задача 1
         System.out.println("Задача 1");
 
-       for (byte i = 1; i < 10; i++) {
+       for (int i = 1; i <= 10; i++) {
             System.out.println(i);
+        }
+        // Задача 2
+        System.out.println();
+        System.out.println("Задача 2");
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+        // Задача 3
+        System.out.println();
+        System.out.println("Задача 3");
+
+        for (int i = 2; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
+        // Задача 4
+        System.out.println();
+        System.out.println("Задача 4");
+
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+
+        // Задача 5
+        System.out.println();
+        System.out.println("Задача 5");
+
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
+        // Задача 6
+        System.out.println();
+        System.out.println("Задача 6");
+
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+        // Задача 7
+        System.out.println();
+        System.out.println("Задача 7");
+
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
+        // Задача 8
+        System.out.println();
+        System.out.println("Задача 8");
+
+        int save = 29_000;
+        int nakoplenia = 0;
+        for (int i = 1; i <= 12; i++) {
+            nakoplenia = nakoplenia + save;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + nakoplenia + " рублей");
+        }
+        // Задача 9
+        System.out.println();
+        System.out.println("Задача 9");
+
+        nakoplenia = 0;
+        for (int i = 1; i <= 12; i++) {
+            nakoplenia = (nakoplenia + nakoplenia / 100) + save;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + nakoplenia + " рублей");
+        }
+        // Задача 10
+        System.out.println();
+        System.out.println("Задача 10");
+
+        int mnogitel = 2;
+        for (int i = 1; i <= 10; i++) {
+            int proizvedenie = mnogitel * i;
+            System.out.println(mnogitel + "*" + i + "=" + proizvedenie);
         }
     }
 }
